46aec98f48caf4a64f82daafadea17cb0c479c17
SobkoSofiya17 <sobkosofiya17@gmail.com>
2022-03-09 10:14:37 +0300

    Initial Commit

---
+++ Schedule/ScheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScheduleApp.swift
+//  Schedule
+//
+//  Created by Тест on 09.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct ScheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
